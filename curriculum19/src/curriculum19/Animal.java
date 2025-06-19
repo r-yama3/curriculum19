ebaebf51972ee73d389d46a0079449f85969f09f
user <user@192.168.1.19>
2025-06-19 16:47:47 +0900

    カリキュラム19

--- curriculum19/src/curriculum19/Animal.java
+++ curriculum19/src/curriculum19/Animal.java
@@ -5,20 +5,19 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class Animal {
-	
+
 	public static void main(String[] args) {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		Dog animal = new Dog ();
-        animal.setDog("ねこ",10);
-        animal.animals();
-		
-		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
-        LocalDate today = LocalDate.now(); // 現在の日付を取得
-        LocalTime nowTime = LocalTime.now(); // 現在の時刻を取得
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        //秒までのフォーマット
-        String formatted = nowTime.format(formatter);
-        System.out.println(today+" "+ formatted); 
+		Dog animal = new Dog("犬", 3);
+		animal.showInfo();
+
+		//現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+		LocalDate today = LocalDate.now(); // 現在の日付を取得
+		LocalTime nowTime = LocalTime.now(); // 現在の時刻を取得
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+		//秒までのフォーマット
+		String formatted = nowTime.format(formatter);
+		System.out.println(today + " " + formatted);
 	}
 }
--- curriculum19/src/curriculum19/Dog.java
+++ curriculum19/src/curriculum19/Dog.java
@@ -1,26 +1,21 @@
 package curriculum19;
 
-public class Dog {
+class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
-	private String animal;
+	String animal;
 
 	// Q2：フィールドに動物の数の変数を定義してください。
-	private int number;
+	int number;
 
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	public Dog() {
-		animal = "犬";
-		number = 0;
-	}
-
-	public void setDog(String animal, int number) { //コンストラクタ作成
-		this.animal = animal;
+	public Dog(String animal, int number) {
+		this.animal = animal = "犬";
 		this.number = number;
 	}
 
-	public void animals() {
+	public void showInfo() {
 		System.out.println(animal);
-		System.out.println("" + number + "匹");
+		System.out.println(number + "匹");
 	}
 }
